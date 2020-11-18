3cf475358bb804293b98944762bfb74e9afd7aaa
Rayyan <63111246+Ryyn-Sd@users.noreply.github.com>
2020-11-18 18:22:10 -0500

    many things

--- src/App.js
+++ src/App.js
@@ -1,41 +1,91 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
-/*
-let's talk in template literal strings
-OK
-or not
-let's talk in multiline comments!
-Or use the chat
-so. what should the app look like? just use html on 12-14 and css in App.css
-also you need the weird <> </> things
+import axios from 'axios'
 
-*/
 function App() {
-  const [ section, setSection ] = useState(0)
+  const [section, setSection] = useState(0)
+  const [area, setArea] = useState(0)
+  // const [woeid, setWoeid] = useState(0)
   return (
     <>
       <ul>
         <li onClick={() => setSection('daily')}>Daily</li>
         <li onClick={() => setSection('hourly')}>Hourly</li>
-        <li className="search"><input type="text" placeholder="Type in a location…"></input></li>
+        <li className="search"><input id="search" type="text" onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            setArea(document.getElementById('search').value)
+            // axios.get(`https://www.metaweather.com/api/location/search/?query=${encodeURIComponent(value)}`)
+            //   .then(res => res.data[0])
+            //   .then(res => {
+            //     value = res.title
+            //     setArea(res.title)
+            //     setWoeid(res.woeid)
+            //   })
+          }
+        }} placeholder="Type in a location…"></input></li>
       </ul>
-      {section === 'daily' ? <Daily /> : null}
-      {section === 'hourly' ? <Hourly /> : null}
+      {section === 'daily' ? <Daily area={area} woeid={woeid}/> : null}
+      {section === 'hourly' ? <Hourly area={area} woeid={woeid} /> : null}
     </>
   );
 }
-function Daily() {
+function Daily(props) {
+  const [weather, setWeather] = useState(Array(14).fill('Loading...'))
+
   return (
     <div className="section daily">
-      Daily:
+      <h1>Daily Weather for {props.area}</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>Day</th>
+            <th>Weather</th>
+          </tr>
+          <tr>
+            <td>Today</td>
+            <td>{weather[0]}</td>
+          </tr>
+          <tr>
+            <td>Tomorrow</td>
+            <td>{weather[1]}</td>
+          </tr>
+          {
+            Array(12).fill().map((_, i) => {
+              return (
+                <tr>
+                  <td>{(new Date(Date.now() + (i + 2) * 24 * 60 * 60 * 1000)).toDateString()}</td>
+                  <td>{weather[i + 2]}</td>
+                </tr>)
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
 
-function Hourly() {
+function Hourly(props) {
+  const [weather, setWeather] = useState(Array(24).fill('Loading...'))
   return (
     <div className="section hourly">
-      Hourly:
+      <h1>Hourly Weather for {props.area}</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>Time</th>
+            <th>Weather</th>
+          </tr>
+          {
+            Array(24).fill().map((_, i) => {
+              return (
+                <tr>
+                  <td>{(new Date(Date.now() + (i) * 60 * 60 * 1000)).getHours() === 0 ? 12 : (new Date(Date.now() + (i) * 60 * 60 * 1000)).getHours() > 12 ? (new Date(Date.now() + (i) * 60 * 60 * 1000)).getHours() - 12 : (new Date(Date.now() + (i) * 60 * 60 * 1000)).getHours()}:00 {(new Date(Date.now() + (i) * 60 * 60 * 1000)).getHours() >= 12 ? "PM" : "AM"}</td>
+                  <td>{weather[i]}</td>
+                </tr>)
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
