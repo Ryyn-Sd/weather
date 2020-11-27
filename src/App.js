dd64988fbec19cd4dd387d72995e913a954ef6cf
Rayyan <63111246+Ryyn-Sd@users.noreply.github.com>
2020-11-27 10:04:01 -0500

    utilities.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-import axios from 'axios'
 import icons from './icons'
-
-const convert = (start, endUnit, round) => {
-  const temp =
-    endUnit && endUnit.toLocaleLowerCase() === 'm'
-      ? start - 273.15
-      : (start - 273.15) * 1.8 + 32
-  return (
-    Math.round(temp).toString() +
-    (endUnit && endUnit.toLocaleLowerCase() === 'm' ? ' ˚C' : ' ˚F')
-  )
-}
-
-const toTimeString = date =>
-  `${
-    new Date(date * 1000).getHours() === 0
-      ? 12
-      : new Date(date * 1000).getHours() > 12
-      ? new Date(date * 1000).getHours() - 12
-      : new Date(date * 1000).getHours()
-  }:${('0' + new Date(date * 1000).getMinutes()).slice(-2)} ${
-    new Date(date * 1000).getHours() >= 12 ? 'PM' : 'AM'
-  }`
-
-const round = (num, amount) =>
-  Math.round(Number(num) * 10 ** (amount || 0)) / 10 ** (amount || 0);
-
-const fetchWeather = async (callback, zip) => {
-  axios
-    .get(zip ? `./api?zip=${encodeURIComponent(zip)}` : `./api`)
-    .then(res => callback(res.data))
-}
-
-const dt = dt =>
-  new Date(dt * 1000).toDateString() === new Date().toDateString()
-    ? 'Today'
-    : new Date(dt * 1000).toDateString() ===
-      new Date(Date.now() + 1 * 24 * 60 * 60 * 1000).toDateString()
-    ? 'Tomorrow'
-    : new Date(dt * 1000).toDateString()
+import { convert, toTimeString, round, fetchWeather, dt } from './utilities'
 
 const App = () => {
   const [section, setSection] = useState('')
