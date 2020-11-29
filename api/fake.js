7ebad96e0df48e0ea786017a3ca7a1840821b723
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-11-29 15:35:37 -0500

    Disable caching on /api/fake

--- api/fake.js
+++ api/fake.js
@@ -383,141 +383,143 @@ const sunriseset = (a, b, c, d = new Date()) => {
   )
 }
 
-const data = {
-  lat: 40.78,
-  lon: -73.98,
-  timezone: 'America/New_York',
-  timezone_offset: -18000,
-  zip: Math.floor(Math.random() * 99450) + 501,
-  current: {
-    dt: Math.floor(Date.now() / 1000),
-    sunrise: Math.floor(sunriseset(40.78, -73.98, true).getTime() / 1000),
-    sunset: Math.floor(sunriseset(40.78, -73.98, false).getTime() / 1000),
-    temp: Math.random() * 50 + 250,
-    feels_like: Math.random() * 50 + 250,
-    pressure: Math.floor(Math.random() * 250 + 850),
-    humidity: Math.floor(Math.random() * 100),
-    dew_point: Math.random() * 15 + 273.15,
-    uvi: Math.floor(Math.random() * 10),
-    clouds: Math.floor(Math.random() * 101),
-    visibility: Math.floor(Math.random() * 10000),
-    wind_speed: Math.floor(Math.random() * 50),
-    wind_deg: Math.floor(Math.random() * 361),
-    weather: [
-      weatherConditions[Math.floor(Math.random() * weatherConditions.length)]
-    ]
-  },
-  minutely: (() => {
-    const result = [
-      {
-        dt: Math.floor(Date.now() / 1000),
-        precipitation: Math.floor(Math.random() * 101)
-      }
-    ]
-    Array(60)
-      .fill()
-      .map((_, i) => {
-        const precipitation = result[result.length - 1].precipitation
-        return result.push({
-          dt: Math.floor(Date.now() / 1000) + 60 * (i + 1),
-          precipitation:
-            precipitation +
-            ((Math.random() > 0.5 || precipitation === 0) &&
-            precipitation !== 100
-              ? 1
-              : -1)
+module.exports = (req, res) => {
+  const data = {
+    lat: 40.78,
+    lon: -73.98,
+    timezone: 'America/New_York',
+    timezone_offset: -18000,
+    zip: Math.floor(Math.random() * 99450) + 501,
+    current: {
+      dt: Math.floor(Date.now() / 1000),
+      sunrise: Math.floor(sunriseset(40.78, -73.98, true).getTime() / 1000),
+      sunset: Math.floor(sunriseset(40.78, -73.98, false).getTime() / 1000),
+      temp: Math.random() * 50 + 250,
+      feels_like: Math.random() * 50 + 250,
+      pressure: Math.floor(Math.random() * 250 + 850),
+      humidity: Math.floor(Math.random() * 100),
+      dew_point: Math.random() * 15 + 273.15,
+      uvi: Math.floor(Math.random() * 10),
+      clouds: Math.floor(Math.random() * 101),
+      visibility: Math.floor(Math.random() * 10000),
+      wind_speed: Math.floor(Math.random() * 50),
+      wind_deg: Math.floor(Math.random() * 361),
+      weather: [
+        weatherConditions[Math.floor(Math.random() * weatherConditions.length)]
+      ]
+    },
+    minutely: (() => {
+      const result = [
+        {
+          dt: Math.floor(Date.now() / 1000),
+          precipitation: Math.floor(Math.random() * 101)
+        }
+      ]
+      Array(60)
+        .fill()
+        .map((_, i) => {
+          const precipitation = result[result.length - 1].precipitation
+          return result.push({
+            dt: Math.floor(Date.now() / 1000) + 60 * (i + 1),
+            precipitation:
+              precipitation +
+              ((Math.random() > 0.5 || precipitation === 0) &&
+              precipitation !== 100
+                ? 1
+                : -1)
+          })
         })
+      return result
+    })(),
+    hourly: Array(48)
+      .fill()
+      .map((v, i) => {
+        return {
+          dt:
+            Math.floor(
+              (() => {
+                const d = new Date()
+                d.setMinutes(0, 0, 0)
+                return d
+              })() / 1000
+            ) +
+            3600 * i,
+          temp: Math.random() * 50 + 250,
+          feels_like: Math.random() * 50 + 250,
+          pressure: Math.floor(Math.random() * 250 + 850),
+          humidity: Math.floor(Math.random() * 100),
+          dew_point: Math.random() * 15 + 273.15,
+          uvi: Math.floor(Math.random() * 10),
+          clouds: Math.floor(Math.random() * 101),
+          visibility: Math.floor(Math.random() * 10000),
+          wind_speed: Math.floor(Math.random() * 50),
+          wind_deg: Math.floor(Math.random() * 361),
+          weather: [
+            weatherConditions[
+              Math.floor(Math.random() * weatherConditions.length)
+            ]
+          ],
+          pop: Number(Math.random() > 0.5)
+        }
+      }),
+    daily: Array(7)
+      .fill()
+      .map((v, i) => {
+        const dt = Math.floor(
+          (() => {
+            const d = new Date()
+            d.setHours(12, 0, 0, 0)
+            return d.getTime()
+          })() /
+            1000 +
+            86400 * i
+        )
+        return {
+          dt: dt,
+          sunrise: Math.floor(
+            sunriseset(40.78, -73.98, true, new Date(dt * 1000)) / 1000
+          ),
+          sunset: Math.floor(
+            sunriseset(40.78, -73.98, false, new Date(dt * 1000)) / 1000
+          ),
+          temp: Object.fromEntries(
+            Array(6)
+              .fill()
+              .sort((a, b) => a - b)
+              .map((v, i) => {
+                return [
+                  ['min', 'morn', 'night', 'eve', 'day', 'max'][i],
+                  Math.random() * 50 + 250
+                ]
+              })
+          ),
+          feels_like: Object.fromEntries(
+            Array(4)
+              .fill()
+              .sort((a, b) => a - b)
+              .map((v, i) => {
+                return [
+                  ['morn', 'night', 'eve', 'day'][i],
+                  Math.random() * 50 + 250
+                ]
+              })
+          ),
+          pressure: Math.floor(Math.random() * 250 + 850),
+          humidity: Math.floor(Math.random() * 100),
+          dew_point: Math.random() * 15 + 273.15,
+          uvi: Math.floor(Math.random() * 10),
+          clouds: Math.floor(Math.random() * 101),
+          wind_speed: Math.floor(Math.random() * 50),
+          wind_deg: Math.floor(Math.random() * 361),
+          weather: [
+            weatherConditions[
+              Math.floor(Math.random() * weatherConditions.length)
+            ]
+          ],
+          pop: Number(Math.random() > 0.5)
+        }
       })
-    return result
-  })(),
-  hourly: Array(48)
-    .fill()
-    .map((v, i) => {
-      return {
-        dt:
-          Math.floor(
-            (() => {
-              const d = new Date()
-              d.setMinutes(0, 0, 0)
-              return d
-            })() / 1000
-          ) +
-          3600 * i,
-        temp: Math.random() * 50 + 250,
-        feels_like: Math.random() * 50 + 250,
-        pressure: Math.floor(Math.random() * 250 + 850),
-        humidity: Math.floor(Math.random() * 100),
-        dew_point: Math.random() * 15 + 273.15,
-        uvi: Math.floor(Math.random() * 10),
-        clouds: Math.floor(Math.random() * 101),
-        visibility: Math.floor(Math.random() * 10000),
-        wind_speed: Math.floor(Math.random() * 50),
-        wind_deg: Math.floor(Math.random() * 361),
-        weather: [
-          weatherConditions[
-            Math.floor(Math.random() * weatherConditions.length)
-          ]
-        ],
-        pop: Number(Math.random() > 0.5)
-      }
-    }),
-  daily: Array(7)
-    .fill()
-    .map((v, i) => {
-      const dt = Math.floor(
-        (() => {
-          const d = new Date()
-          d.setHours(12, 0, 0, 0)
-          return d.getTime()
-        })() /
-          1000 +
-          86400 * i
-      )
-      return {
-        dt: dt,
-        sunrise: Math.floor(
-          sunriseset(40.78, -73.98, true, new Date(dt * 1000)) / 1000
-        ),
-        sunset: Math.floor(
-          sunriseset(40.78, -73.98, false, new Date(dt * 1000)) / 1000
-        ),
-        temp: Object.fromEntries(
-          Array(6)
-            .fill()
-            .sort((a, b) => a - b)
-            .map((v, i) => {
-              return [
-                ['min', 'morn', 'night', 'eve', 'day', 'max'][i],
-                Math.random() * 50 + 250
-              ]
-            })
-        ),
-        feels_like: Object.fromEntries(
-          Array(4)
-            .fill()
-            .sort((a, b) => a - b)
-            .map((v, i) => {
-              return [
-                ['morn', 'night', 'eve', 'day'][i],
-                Math.random() * 50 + 250
-              ]
-            })
-        ),
-        pressure: Math.floor(Math.random() * 250 + 850),
-        humidity: Math.floor(Math.random() * 100),
-        dew_point: Math.random() * 15 + 273.15,
-        uvi: Math.floor(Math.random() * 10),
-        clouds: Math.floor(Math.random() * 101),
-        wind_speed: Math.floor(Math.random() * 50),
-        wind_deg: Math.floor(Math.random() * 361),
-        weather: [
-          weatherConditions[
-            Math.floor(Math.random() * weatherConditions.length)
-          ]
-        ],
-        pop: Number(Math.random() > 0.5)
-      }
-    })
+  }
+  res.setHeader('Cache-Control', 'no-cache, no-store')
+  res.json(data)
 }
-
-module.exports = (req, res) => res.json(data)
